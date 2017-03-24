cc9f19131e26636b3441bff3f1b3b19ddd0053b8
Josef Ottosson <josef.ottosson@creuna.se>
2017-03-24 15:12:46 +0100

    Initial commit

---
+++ StreamIssue/StreamIssue/JsonValidationMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+using Newtonsoft.Json.Linq;
+
+namespace StreamIssue {
+	public class JsonValidatorMiddleware {
+		private readonly RequestDelegate next;
+		private static readonly HttpMethod[] RequiredVerbs = { HttpMethod.Post, new HttpMethod("PATCH") };
+
+		public JsonValidatorMiddleware(RequestDelegate next) {
+			this.next = next;
+		}
+
+		public async Task Invoke(HttpContext context) {
+			var httpVerb = new HttpMethod(context.Request.Method);
+			if(!RequiredVerbs.Contains(httpVerb)) {
+				await this.next.Invoke(context);
+				return;
+			}
+
+			var initalBody = context.Request.Body;
+			var requestBody = new StreamReader(context.Request.Body).ReadToEnd();
+
+			bool success;
+			try {
+				JToken.Parse(requestBody);
+				success = true;
+			} catch(Exception) {
+				success = false;
+			}
+			
+			if(success) {
+				initalBody.Seek(0, SeekOrigin.Begin);
+				context.Request.Body = initalBody;
+				await this.next.Invoke(context);
+			} else {
+				context.Response.StatusCode = 400;
+				await context.Response.WriteAsync("Not valid!");
+			}
+		}
+	}
+
+	public static class MiddlewareApplicationBuilderExtensions {
+		public static IApplicationBuilder UseJsonValidator(this IApplicationBuilder builder) {
+			return builder.UseMiddleware<JsonValidatorMiddleware>();
+		}
+	}
+}
---
+++ StreamIssue/Tests/Tests.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+
+using Shouldly;
+
+using Xunit;
+
+namespace StreamIssue
+{
+    public class Tests
+    {
+		[Fact]
+	    public async Task GivenInvalidJson_WhenPost_ThenShouldReturnError() {
+		    var testServer = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+		    var client = testServer.CreateClient();
+			var postMessage = new HttpRequestMessage(HttpMethod.Post, "/");
+			postMessage.Content = new StringContent("{");
+		    var response = await client.SendAsync(postMessage);
+
+		    response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+	    }
+
+	    [Fact]
+	    public async Task GivenValidJson_WhenPost_ThenShouldReturnOk() {
+		    var testServer = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+		    var client = testServer.CreateClient();
+		    var postMessage = new HttpRequestMessage(HttpMethod.Post, "/");
+		    postMessage.Content = new StringContent("{}");
+		    var response = await client.SendAsync(postMessage);
+
+		    response.StatusCode.ShouldBe(HttpStatusCode.OK);
+	    }
+	}
+}
